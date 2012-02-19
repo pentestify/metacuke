e52c7a363088d30daf972627a5a4a19e52ceaf82
Jonathan Cran <jcran@0x0e.org>
2012-02-19 12:45:00 -0600

    add known_nets generation

--- utils/configure.rb
+++ utils/configure.rb
@@ -22,13 +22,22 @@ env_path = File.join(data_path, env)
 FileUtils.mkdir(env_path) unless File.directory? env_path
 FileUtils.mkdir(data_path) unless File.directory? data_path
 
-f = File.open(File.join(env_path, "known_systems.txt"),"w")
 
+#
+# Populate known nets
+#
+known_nets = File.open(File.join(env_path, "known_nets.txt"),"w")
+known_nets.puts range.join("\n")
+
+#
+# Populate known systems
+#
+known_systems = File.open(File.join(env_path, "known_systems.txt"),"w")
 parser = Nmap::Parser.parsefile(nmap_output_path)
-
 parser.hosts("up").each do |host|
   puts "found host #{host.addr}"
-  f.puts host.addr
+  known_systems.puts host.addr
 end
 
 puts "done."
+
